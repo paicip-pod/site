190b78e1d6ecc4b40e95e54a41f0f314a90b66bf
paicip-pod <paicippod@gmail.com>
2023-01-23 18:09:11 +0200

    Basic template working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  socials: any[] = [ { handle: 'https://www.instagram.com/paicip_pod/', icon: 'fab fa-instagram ' } ];
+  albums: any[] = [
+    { title: '1' }, { title: '2' },{ title: '3' }
+  ];
   title = 'site';
 }
